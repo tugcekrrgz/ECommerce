4d614326fd5444682edaf6418fa71c9595391bd6
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-02 22:17:09 +0300

    Veritabanı oluşturuldu.

---
+++ ECommerce.DAL/Context/ECommerceContext.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Entity.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.DAL.Context
+{
+    internal class ECommerceContext:IdentityDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=DESKTOP-I9V1GOS\\SQLEXPRESS;database=ECommerceDB;uid=sa;pwd=Tugce8417058;TrustServerCertificate=True");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
