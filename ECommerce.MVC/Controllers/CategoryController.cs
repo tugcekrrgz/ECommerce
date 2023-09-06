c1421acd84982fc059f42743c5f36088499f0e75
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-07 01:08:45 +0300

    Kategoriye göre ürün getirme işlemi yapıldı.

---
+++ ECommerce.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.BLL.AbstractServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public CategoryController(IProductService productService) 
+        {
+            _productService = productService;
+        }
+        public IActionResult GetProducts(int id)
+        {
+            var products=_productService.GetAllProducts().Where(x => x.CategoryId == id).ToList();
+            return View(products);
+        }
+    }
+}
