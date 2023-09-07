5226d0ed865279d69399aa664713ad6ed7a48c7c
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-07 12:09:27 +0300

    Order class tanımlandı.

---
+++ ECommerce.Entity/Entity/Order.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Entity.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.Entity.Entity
+{
+    internal class Order:BaseClass
+    {
+        public DateTime? ShippedDate { get; set; }
+        public int UserId { get; set; }
+        public virtual IdentityUser User { get; set; }
+    }
+}
