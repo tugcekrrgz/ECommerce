782767ca0a828f350fc9f18a2105daf1438e4555
tugcekrrgz <123090853+tugcekrrgz@users.noreply.github.com>
2023-09-08 21:43:35 +0300

    Sipariş detayları sayfası oluşturuldu.

---
+++ ECommerce.MVC/Areas/Dashboard/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.BLL.AbstractServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.MVC.Areas.Dashboard.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly IOrderDetailService _orderDetailService;
+        private readonly IProductService _productService;
+
+        public OrderController(IOrderService orderService, IOrderDetailService orderDetailService, IProductService productService) 
+        {
+            _orderService = orderService;
+            _orderDetailService = orderDetailService;
+            _productService = productService;
+        }
+
+        [Area("Dashboard")]
+        public IActionResult Details(int id)
+        {
+            var order=_orderService.GetById(id);
+            ViewBag.OrderDetails = _orderDetailService.GetAllOrderDetails();
+            ViewBag.Products= _productService.GetAllProducts();
+            return View(order);
+        }
+    }
+}
